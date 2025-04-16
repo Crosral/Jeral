3d1e7bfd983297651e5603224fc6ea2cabed29ee
Crosral <jeral.melillancachavez@gmail.com>
2025-04-16 16:46:16 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,80 @@
+
+# nombre="diego"
+# edad=45
+
+# print("hola", nombre ,"su edad es", edad)
+
+# #solicitud de datos
+# print("ingrese su nombre")
+# nombre=input()
+# print("ingrese su edad")
+# edad=input()
+
+# #ejemplo de concatenación
+# print("hola", nombre ,"su edad es", edad)
+
+
+
+# print("ingrese un numero")
+
+# n1=int(input())
+
+# print("ingrese otro numero")
+
+# n2=int(input())
+
+# print("ingrese otro numero")
+
+# n3=int(input())
+
+# n4=3
+
+# prom=n1+n2+n3
+
+# total=prom/n4
+
+# print("el promedio de los numeros son", total)
+
+
+# if total>-40:
+#     print("el alumno aprobo")
+# else:
+#     print("el alumno reprobo")
+
+
+
+
+
+# print("escriba su edad usuario")
+# n1=int(input())
+
+# if n1>=40:
+#     print("usted es mayor quitese")
+# else:
+#     print("que joven muchacho")
+
+
+
+
+# print("escriba su edad usuario")
+# edad=input()
+
+# if edad<12:
+#     print("usted es niño quitese")
+# elif edad>=12 and edad<=17:
+#     print
+# elif edad>=18 and edad<65:
+#     print("flaco uste es adulto")
+
+print("escriba 3 numeros")
+n1=input()
+n2=input()
+n3=input()
+
+if n1>=n2 and n1>n3:
+    print("el numero mayor es", n1)
+elif n2>n3:
+    print("el numero mayor es", n2)
+else:
+    print("el numero mayor es", n3) 
+
