d18a08c1da3b7b246149a85110ed5bc0c54a54af
Crosral <jeral.melillancachavez@gmail.com>
2025-04-21 11:20:12 -0400

    for 2

--- 21-04.py
+++ 21-04.py
@@ -63,3 +63,15 @@
 # else:
 #       print("reprobaste pendejo")
     
+
+
+
+
+
+
+num=int(input())
+
+if num % 2==0:
+    print("el numero ", num ," es par")
+else:
+    print("el numero ", num , " es impar")
