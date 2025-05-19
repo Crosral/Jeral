5c194a5192cbf9425b09b061301373f85a4b60fc
Crosral <jeral.melillancachavez@gmail.com>
2025-05-19 11:11:16 -0400

    Create 19-05.py

---
+++ 19-05.py
@@ -0,0 +1,84 @@
+
+import random
+
+# while True:
+#     try:
+#         cant=int(input("ingrese un numero de perros"))
+
+#         cuota=3
+
+#         for i in range(cant):
+#             conj=random.randint(0,5)
+#             print(f"el perro {i+1} trajo {conj} conejos")
+#             if conj>=cuota:
+#                 print("el perro gana filete")
+#                 cumple+=1
+#             else:
+#                 print("se queda sin filete muejeje")
+            
+
+#         print(f"la cantidad de perros que cumplieron la cuota es {cumple}")
+#         print(f"la cantidad de perros que no cumplieron la cuota es {cant-cumple}")
+#     except Exception:
+#         print("solo se aceptan numeros enteros")
+
+
+
+
+# tal=int(input("ingrese la cantidad de talleres que asistio "))
+
+# roj=int(input("ingrese la cantidad de rojos que tienes "))
+
+# dec=0
+
+# for i in range(tal):
+#     dec=dec+0.2
+
+# if dec==1:
+#     print("tienes la bendición del profe pe causa")
+# else:
+#     print("ya sacaste 2 xd")
+
+# total=dec+
+
+# rojos=int("diga la cantidad de rojos ")
+
+# talleres=4
+
+# decimas=0
+
+# for r in range(rojos):
+#     for t in range(talleres):
+#         asist=input(f"asistio al taller numero {1+r} 1.- s1, 2 , no")
+#         if asist.lower()=="si":
+#             decimas+=0.3
+#     if decimas>=1:
+#         print("tienes la bendición del profe")
+#     else:
+#         print("ya valiste carnal")
+#     nf=float(input("ingrese su notal final"))
+#     nf+=decimas
+#     if nf>=4:
+#         print(f"el alumno aprobo, su nota es {nf}")
+#     else:
+#         print("reprobo el inutil ese")
+
+
+
+
+
+# def lav():
+
+#     print('''
+
+#         1.-cursar pago del lavado
+#         2.-ver ventas diarias
+#         3.-salir
+
+#             ''')
+    
+#     opc=int(input("elige una opcion"))
+
+
+
+
