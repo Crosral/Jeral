59b27eddacc72a8a1da5cbe7125e271e735a7f3b
Crosral <jeral.melillancachavez@gmail.com>
2025-06-09 11:19:05 -0400

    Create 09-06.py

---
+++ 09-06.py
@@ -0,0 +1,97 @@
+
+
+
+# numeros=[2,5,12,8,77,6]
+# #        0 1  2 3  4
+# print(numeros)
+# inten=int(input("cuantas veces quiere llegar a un numero "))
+
+# for i in range(inten):
+#     op=int(input("escriba un numero para ver algun numero exacto "))
+#     print(numeros[op])
+
+# numeros=[2,5,12,8,77]
+#        0 1  2 3  4
+
+# for numeros in numeros:
+#     print("numero x 2", numeros*2)
+
+
+# nombres=["felipe", "curly", "larry", "moe"]
+
+# print(nombres)
+
+# nombres.append("luthien")
+
+# print(nombres)
+
+# n=["christopher", "edwar", "jhon", "hanz"]
+# a=["nolan", "norton", "williams", "zimmer"]
+
+# c=0
+
+# for i in a:
+#     print(n[c], a[c])
+#     c+=1
+
+
+
+
+
+
+
+
+# noms=[]
+# apellido=[]
+
+
+# while True:
+#     print('''   
+
+#           1.- ingresar nombre
+#           2.- mostrar nombres y apellidos
+#           3.- salir
+
+
+#             ''')
+    
+#     op=int(input())
+
+#     match op:
+#         case 1:
+#             nom=input("ingrese su nombre ")
+#             noms.append(nom)
+#             ape=input("ingrese su apellido ")
+#             apellido.append(ape)
+#         case 2:
+#             c=0
+#             for i in noms:
+#                 print(noms[c], apellido[c])
+#                 c+=1
+
+#         case 3:
+#             busca=input("indique que nombre buscara: ")
+#             if busca in noms:
+#                 print(f"el nombre {busca} esta en la lista")
+#             else:
+#                 print(f"el nombre {busca} no esta en la lista")
+
+#         case 4:
+#             print("saliendo...")
+#             break
+#         case _:
+#             print("escoga una opcion valida")
+
+
+
+print('''
+
+      1.-
+      2.-
+      3.-
+        ''')
+
+
+
+
+
